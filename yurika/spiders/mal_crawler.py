a97d28f3823be23d953d6a0576aeab1712a58ac0
frosty <j4qfrost@gmail.com>
2017-08-26 17:29:51 -0700

    stress testing; make proxy listing more dynamic/handle timeouts

--- yurika/spiders/mal_crawler.py
+++ yurika/spiders/mal_crawler.py
@@ -12,16 +12,18 @@ class MalCrawlerSpider(scrapy.Spider):
 	with open('yurika/proxy_list.txt','r') as f:
 		proxy_list = [l.strip() for l in f.readlines()]
 	missed_urls = []
+	max_request = 500
+	anime_link = 'https://myanimelist.net/anime/'
+
 
 	def __init__(self, *args, **kwargs):
 		logger = logging.getLogger('scrapy')
-		logger.setLevel(logging.INFO)
+		logger.setLevel(logging.DEBUG)
 		super().__init__(*args, **kwargs)
 
 	def parse(self, response):
-		link = 'https://myanimelist.net/anime/'
-		for i in range(1,500):
-			request = scrapy.Request(url=link+str(i), 
+		for i in range(1,17):
+			request = scrapy.Request(url=MalCrawlerSpider.anime_link+str(i), 
 									callback=self.grab_data, 
 									errback=self.handle_miss, 
 									dont_filter=True, 
@@ -36,6 +38,14 @@ class MalCrawlerSpider(scrapy.Spider):
 		item['_id'] = response.meta['id']
 		item['title'] = response.css('.h1').xpath('./span/text()').extract()[0]
 		yield item
+		if response.meta['id']+16 <= MalCrawlerSpider.max_request:
+			request = scrapy.Request(url=MalCrawlerSpider.anime_link+str(response.meta['id']+16), 
+									callback=self.grab_data, 
+									errback=self.handle_miss, 
+									dont_filter=True, 
+									meta={'id': response.meta['id']+16})
+			request.meta['proxy'] = 'https://' + random.choice(MalCrawlerSpider.proxy_list)
+			yield request
 
 	def handle_miss(self, failure):
 		if failure.check(HttpError):
@@ -47,4 +57,13 @@ class MalCrawlerSpider(scrapy.Spider):
 									errback=self.handle_miss, 
 									dont_filter=True, 
 									meta={'id': failure.value.response.meta['id']})
+			elif failure.value.response.status == 404:
+				if failure.value.response.meta['id']+16 <= MalCrawlerSpider.max_request:
+					request = scrapy.Request(url=MalCrawlerSpider.anime_link+str(failure.value.response.meta['id']+16), 
+											callback=self.grab_data, 
+											errback=self.handle_miss, 
+											dont_filter=True, 
+											meta={'id': failure.value.response.meta['id']+16})
+					request.meta['proxy'] = 'https://' + random.choice(MalCrawlerSpider.proxy_list)
+					yield request
 
