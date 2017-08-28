69011d3e0633a98c772388c0f842aadfa2a1c940
frosty <j4qfrost@gmail.com>
2017-08-27 17:49:53 -0700

    ready to crawl up pipes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Yurika-scraper
+
+run scrapy crawl mal_crawler in the project root
+the file you want to modify will probably be yurika/spiders/mal_crawler.py
+will format this REACME better at a later date
+GoT time!
--- yurika/spiders/mal_crawler.py
+++ yurika/spiders/mal_crawler.py
@@ -4,6 +4,7 @@ from yurika.items import YurikaItem
 from scrapy.spidermiddlewares.httperror import HttpError
 from twisted.internet.error import DNSLookupError
 from twisted.internet.error import TimeoutError
+from twisted.internet.error import TCPTimedOutError
 import random
 import logging
 
@@ -13,66 +14,66 @@ class MalCrawlerSpider(scrapy.Spider):
 	start_urls = ['https://myanimelist.net/']
 	with open('yurika/proxy_list.txt','r') as f:
 		proxy_list = [l.strip() for l in f.readlines()]
-	missed_urls = []
-	max_request = 500
+	check_proxies = []
+	max_series = 500
 	anime_link = 'https://myanimelist.net/anime/'
+	requests = []
 
 
 	def __init__(self, *args, **kwargs):
 		logger = logging.getLogger('scrapy')
-		logger.setLevel(logging.DEBUG)
+		logger.setLevel(logging.INFO)
 		super().__init__(*args, **kwargs)
+		self.generate_requests()
 
 	def parse(self, response):
 		for i in range(1,17):
-			request = scrapy.Request(url=MalCrawlerSpider.anime_link+str(i), 
-									callback=self.grab_data, 
-									errback=self.handle_miss, 
-									dont_filter=True, 
-									meta={'id': i})
-			request.meta['proxy'] = 'https://' + random.choice(MalCrawlerSpider.proxy_list)
-			yield request
+			yield MalCrawlerSpider.requests.pop()
+
+	def generate_requests(self):
+		if not MalCrawlerSpider.requests:
+			for i in range(1,MalCrawlerSpider.max_series):
+				request = scrapy.Request(url=MalCrawlerSpider.anime_link+str(i), 
+										callback=self.grab_data, 
+										errback=self.handle_miss, 
+										dont_filter=True, 
+										meta={'id': i})
+				request.meta['proxy'] = 'https://' + random.choice(MalCrawlerSpider.proxy_list)
+				MalCrawlerSpider.requests.append(request)
 
 	def grab_data(self, response):
-	
 		item = YurikaItem()
 		#TODO: add more properties
 		item['_id'] = response.meta['id']
 		item['title'] = response.css('.h1').xpath('./span/text()').extract()[0]
 		yield item
-		if response.meta['id']+16 <= MalCrawlerSpider.max_request:
-			request = scrapy.Request(url=MalCrawlerSpider.anime_link+str(response.meta['id']+16), 
-									callback=self.grab_data, 
-									errback=self.handle_miss, 
-									dont_filter=True, 
-									meta={'id': response.meta['id']+16})
-			request.meta['proxy'] = 'https://' + random.choice(MalCrawlerSpider.proxy_list)
-			yield request
+		if MalCrawlerSpider.requests:
+			yield MalCrawlerSpider.requests.pop()
 
 	def handle_miss(self, failure):
 		logger = logging.getLogger('scrapy')
 		if failure.check(HttpError):
+			#logger.error(failure.value.response.status)
 			if failure.value.response.status == 429:
 				logger.info("Retrying entry..." + failure.value.response.url)
-				yield scrapy.Request(url=failure.value.response.url,
+				request = scrapy.Request(url=failure.value.response.url,
 									callback=self.grab_data, 
 									errback=self.handle_miss, 
 									dont_filter=True, 
 									meta={'id': failure.value.response.meta['id']})
+				request.meta['proxy'] = 'https://' + random.choice(MalCrawlerSpider.proxy_list)
+				yield request
 			elif failure.value.response.status == 404:
-				if failure.value.response.meta['id']+16 <= MalCrawlerSpider.max_request:
-					request = scrapy.Request(url=MalCrawlerSpider.anime_link+str(failure.value.response.meta['id']+16), 
-											callback=self.grab_data, 
-											errback=self.handle_miss, 
-											dont_filter=True, 
-											meta={'id': failure.value.response.meta['id']+16})
-					request.meta['proxy'] = 'https://' + random.choice(MalCrawlerSpider.proxy_list)
-					yield request
-		elif failure.check(DNSLookupError):
-			request = failure.request
-			logger.error('DNSLookupError on %s', request.url)
+				pass
+		elif failure.check(TCPTimedOutError,TimeoutError):
+			logger.error("Check connectivity and that proxy " 
+						+ failure.request.meta['proxy']
+						+ " is up and not blocked.")
+			MalCrawlerSpider.check_proxies.append(failure.request.meta['proxy'] + '\n')
 
-		elif failure.check(TimeoutError):
-			request = failure.request
-			logger.error('TimeoutError on %s', request.url)
+	def spider_closed(self, spider):
+		with open('yurika/proxy_list.txt','w') as f:
+			for p in MalCrawlerSpider.proxy_list:
+				if not p in MalCrawlerSpider.check_proxies:
+					f.write(p)
 
